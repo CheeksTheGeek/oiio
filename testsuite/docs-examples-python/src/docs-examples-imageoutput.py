27e36bf5f1a4d4d9526f9d6cacfa3a3fe3d434f9
Chaitanya Sharma <39400946+CheeksTheGeek@users.noreply.github.com>
2023-10-12 17:57:46 -0400

    added working python test for Writing Tiles in ImageOutput
    Signed-off-by: Chaitanya Sharma <39400946+CheeksTheGeek@users.noreply.github.com>

--- testsuite/docs-examples-python/src/docs-examples-imageoutput.py
+++ testsuite/docs-examples-python/src/docs-examples-imageoutput.py
@@ -81,6 +81,49 @@ def scanlines_write() :
         out.close ()
         # END-imageoutput-scanlines
 
+# BEGIN-imageoutput-tilewriting
+import OpenImageIO as oiio
+import numpy as np
+import os
+
+def tiles_write():
+    filename = "test_tile_output.tif"  # Assume tiff supports tiles
+    xres, yres, channels, tilesize = 128, 128, 3, 64
+    out = oiio.ImageOutput.create(filename)
+    
+    if not out.supports("tiles"):
+        print("Tiles are not supported for this image format.")
+        return
+    
+    spec = oiio.ImageSpec(xres, yres, channels, oiio.UINT8)
+    spec.tile_width = tilesize
+    spec.tile_height = tilesize
+    out.open(filename, spec)
+    
+    # Generating data in tile, for simplicity filling it with a constant value.
+    tile = np.full((tilesize, tilesize, channels), 255, dtype=np.uint8)
+    
+    z = 0  # Always zero for 2D images
+    for y in range(0, yres, tilesize):
+        for x in range(0, xres, tilesize):
+            # out.write_tile(x, y, z, oiio.TypeDesc(oiio.UINT8), tile.tobytes())
+            out.write_tile(x, y, z, tile.tobytes())
+    
+    out.close()
+    
+    # Now to verify the data
+    inp = oiio.ImageInput.open(filename)
+    for y in range(0, yres, tilesize):
+        for x in range(0, xres, tilesize):
+            read_tile = inp.read_tile(x, y, z, oiio.UINT8)
+            assert np.array_equal(tile, np.array(read_tile))
+    
+    inp.close()
+    
+    # Cleanup
+    os.remove(filename)
+# END-imageoutput-tilewriting
+
 
 
 if __name__ == '__main__':
@@ -88,3 +131,4 @@ if __name__ == '__main__':
     # as part of the test.
     simple_write()
     scanlines_write()
+    tiles_write()
